958bfc6c8dd6b97edf16ce67c399c4a42ebc199f
Evgenii Bykovskii <45427494+Eugene-Bykovsky@users.noreply.github.com>
2023-11-15 00:19:01 +0400

    add Task Admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'body',
+        'priority'
+    )
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.site_header = 'Админпанель ToDo'
--- todos/models.py
+++ todos/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
+    priority_choices = [
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High')
+    ]
+    priority = models.CharField(max_length=10, choices=priority_choices)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['priority']
+
+    def __str__(self):
+        return self.name
