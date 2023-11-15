a332e7899d1ce87583a4fdc9851ed91c11f95ee0
Evgenii Bykovskii <45427494+Eugene-Bykovsky@users.noreply.github.com>
2023-11-16 01:33:47 +0400

    add SubTask model

--- todos/models.py
+++ todos/models.py
@@ -5,16 +5,31 @@ class Task(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
     priority_choices = [
-        (1, 'Low'),
-        (2, 'Medium'),
-        (3, 'High')
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High')
     ]
     priority = models.CharField(max_length=10, choices=priority_choices)
 
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
-        ordering = ['priority']
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class SubTask(models.Model):
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Подзадача'
+        verbose_name_plural = 'Подзадачи'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
