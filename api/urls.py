9bd4589daa97f94538c054aa7a442bbb75b0e8ae
Evgenii Bykovskii <45427494+Eugene-Bykovsky@users.noreply.github.com>
2023-11-30 22:16:26 +0400

    Revert "add swagger"
    This reverts commit e8b72a36a20b19fc1a3efff797dae6efacb5412c.

--- api/urls.py
+++ api/urls.py
@@ -3,28 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from .views import TaskViewSet, SubTaskViewSet
 
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
-
 app_name = 'api'
 
 router = DefaultRouter()
 
-router.register(r'v1/tasks', TaskViewSet)
-router.register(r'v1/subtasks', SubTaskViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'subtasks', SubTaskViewSet)
 
-docs_urlpatterns = [
-    path("v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "v1/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="api:schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "v1/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="api:schema"),
-        name="redoc",
-    ),
-]
-
-urlpatterns = router.urls + docs_urlpatterns
+urlpatterns = (
+    path('', include(router.urls)),
+)
